3561c30c8ec10e3142e88dadd23cf62941dd6385
Christian Ramirez <christian.r0602@gmail.com>
2022-06-13 12:35:27 -0500

    setting up server and client side

---
+++ client/src/components/ProductForm.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+const ProductForm = (e) => {
+  // e.preventDefault()
+  return (
+    <div>
+      <h2>Hello Products</h2>
+    </div>
+  )
+}
+
+
+export default ProductForm
