ce9352a661a7a42f68f7f3373d64ce4c3428bb32
Christian Ramirez <christian.r0602@gmail.com>
2022-06-13 12:47:50 -0500

    tracking changes on input

--- client/src/components/ProductForm.js
+++ client/src/components/ProductForm.js
@@ -3,21 +3,24 @@ import axios from 'axios'
 
 const ProductForm = (e) => {
   // e.preventDefault()
+  const [title, setTitle] = useState('')
+  const [price, setPrice] = useState('')
+  const [description, setDescription] = useState('')
   return (
     <div>
       <h1>Product Form</h1>
       <form>
         <div>
           <label>Title</label>
-          <input type='text'></input>
+          <input type='text' onChange={e => setTitle(e.target.value)}></input>
         </div>
         <div>
           <label>Price</label>
-          <input type='text'></input>
+          <input type='text' onChange={e => setPrice(e.target.value)}></input>
         </div>
         <div>
           <label>Description</label>
-          <input type='text'></input>
+          <input type='text' onChange={e => setDescription(e.target.value)}></input>
         </div>
         <button>Create</button>
       </form>
