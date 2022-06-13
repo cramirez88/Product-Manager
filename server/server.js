a1d34f42eff92311fb5c3fb683e5b9920e2f5b67
Christian Ramirez <christian.r0602@gmail.com>
2022-06-13 12:38:27 -0500

    configuring database

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = 8000;
 const productRoutes = require('./routes/product.routes')
 productRoutes(app)
+app.use(cors())
+
 
     
 app.listen(port, () => console.log(`Listening on port: ${port}`) );
