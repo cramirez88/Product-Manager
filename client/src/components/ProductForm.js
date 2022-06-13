76e68b86288622fc1019b6ff6c1d5a4c9d962be0
Christian Ramirez <christian.r0602@gmail.com>
2022-06-13 12:42:11 -0500

    template for form component

--- client/src/components/ProductForm.js
+++ client/src/components/ProductForm.js
@@ -5,8 +5,24 @@ const ProductForm = (e) => {
   // e.preventDefault()
   return (
     <div>
-      <h2>Hello Products</h2>
+      <h1>Product Form</h1>
+      <form>
+        <div>
+          <label>Title</label>
+          <input type='text'></input>
+        </div>
+        <div>
+          <label>Price</label>
+          <input type='text'></input>
+        </div>
+        <div>
+          <label>Description</label>
+          <input type='text'></input>
+        </div>
+        <button>Create</button>
+      </form>
     </div>
+    
   )
 }
 
