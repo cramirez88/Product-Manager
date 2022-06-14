81d4d51b1687a6303c814ddb67216715eca858b0
Christian Ramirez <christian.r0602@gmail.com>
2022-06-13 23:18:33 -0500

    client side set up with axios

--- client/src/components/ProductForm.js
+++ client/src/components/ProductForm.js
@@ -1,26 +1,47 @@
 import React, {useState} from 'react'
 import axios from 'axios'
 
-const ProductForm = (e) => {
-  // e.preventDefault()
+const ProductForm = () => {
+
   const [title, setTitle] = useState('')
   const [price, setPrice] = useState('')
   const [description, setDescription] = useState('')
+
+
+  const submitHandler = e => {
+    e.preventDefault()
+
+    axios.post('http://localhost:8000/api/product', {
+      title,
+      price,
+      description
+    })
+    .then(res => {
+      console.log(res.data)
+      setTitle('')
+      setPrice('')
+      setDescription('')
+    })
+    .catch(err => console.log(err))
+  }
+
+
+
   return (
     <div>
       <h1>Product Form</h1>
-      <form>
+      <form onSubmit={submitHandler} >
         <div>
           <label>Title</label>
-          <input type='text' onChange={e => setTitle(e.target.value)}></input>
+          <input type='text' onChange={e => setTitle(e.target.value)} value={title}></input>
         </div>
         <div>
           <label>Price</label>
-          <input type='text' onChange={e => setPrice(e.target.value)}></input>
+          <input type='text' onChange={e => setPrice(e.target.value)} value={price}></input>
         </div>
         <div>
           <label>Description</label>
-          <input type='text' onChange={e => setDescription(e.target.value)}></input>
+          <input type='text' onChange={e => setDescription(e.target.value)} value={description}></input>
         </div>
         <button>Create</button>
       </form>
