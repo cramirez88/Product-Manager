ed51600d521d09d7b4c79ae98239cf5bb83f1c23
Christian Ramirez <christian.r0602@gmail.com>
2022-06-21 11:51:14 -0500

    form

--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import ProductForm from '../components/ProductForm'
 import ProductList from '../components/ProductList'
@@ -6,13 +6,36 @@ import ProductList from '../components/ProductList'
 const Main = props => {
   const [title, setTitle] = useState([])
 
-  const removeProduct = id => {
-    setTitle(title.filter(title => title._id !== id))
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/product')
+    .then(res => {
+      setTitle(res.data)
+    })
+    .catch((err) => console.log(err))
+  },[])
+
+  const removeProduct = productId => {
+    axios.delete('http://localhost:8000/api/product' + productId)
+    .then((res) => {
+      console.log(res)
+      console.log(res.data)
+      setTitle(title.filter(title => title._id !== productId))
+    })
+    .catch((err) => console.log(err))
   }
 
+const createProduct = productParam => {
+  axios.post('http://localhost:8000/api/product' + productParam)
+  .then(res => {
+    console.log(res)
+    console.log(res.data)
+    setTitle([...title, res.data])
+  })
+  .catch((err) => console.log(err))
+}
   return (
     <div>
-      <ProductForm title={title} setTitle={setTitle} />
+      <ProductForm onSubmitProp={createProduct} initialName='' title={title} setTitle={setTitle} />
       <ProductList title={title} setTitle={setTitle} removeProduct={removeProduct} />
     </div>
   )
